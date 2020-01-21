1c55ad3df38111c562338e8421579df3e22848d4
José Santamaría <santamaria0323@gmail.com>
2020-01-21 20:03:13 -0300

    navbar 2

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <div id="app">
     <div>
       <b-navbar toggleable="lg" type="dark" variant="info">
-        <img class="mr-4" src="https://image.flaticon.com/icons/png/512/82/82455.png" width="80px" id="img_logomaster">
-        <b-navbar-brand href="#" class="" >Peliculas</b-navbar-brand>
+        <img class="mr-4" src="./assets/image.png" width="80px" id="img_logomaster">
+        <!--<b-navbar-brand href="#" class="" >Peliculas</b-navbar-brand>-->
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item href="/populares">Populares</b-nav-item>
+            <b-nav-item href="/">Populares</b-nav-item>
             
           </b-navbar-nav>
 
