dd045aa0dc73e9fc11fb43fcfa10f2dfda2e10ce
José Santamaría <santamaria0323@gmail.com>
2020-01-22 13:24:42 -0300

    busqueda pelicula

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
 
           <b-navbar-nav class="ml-auto">
             <b-nav-form>
-              <b-form-input size="sm" class="mr-sm-2" v-model="busqueda" placeholder="Buscar"></b-form-input>
+              <b-form-input size="sm" class="mr-sm-2" v-model="busqueda" v-on:change="buscar" placeholder="Buscar"></b-form-input>
               <b-button size="sm" class="my-2 my-sm-0" variant="dark"><b-icon icon="search" v-on:click="buscar"></b-icon></b-button>
             </b-nav-form>
           </b-navbar-nav>
