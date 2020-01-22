54267ad69b35045d90bad0e24b550b4da5f570dc
Danilo <ibanez3497@gmail.com>
2020-01-21 23:55:55 -0300

    Resolver id erroneo

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,31 +1,36 @@
 <template>
   <div class="container">
-    <b-card :img-src="movie2.poster" img-alt="Card image" img-left class="mb-3" img-height="400">
-      <b-card-text>
-        <h2>{{movie.title}} ({{movie2.date}})</h2><hr>
-        <p> {{movie.overview}} </p><hr>
-        <genres v-bind:genres="genres" /> <hr>
-        <h3><b-icon icon="star-fill" variant="warning">
-          </b-icon>{{" " + movie.vote_average}}<h6>Votos: {{movie.vote_count}}</h6>
-        </h3> 
-        <a :href="movie.homepage">Página oficial de la película</a>
-      </b-card-text>
-    </b-card>
+    <div v-if="mostrar">
+      <b-card :img-src="movie2.poster" img-alt="Card image" img-left class="mb-3" img-height="400">
+        <b-card-text>
+          <h2>{{movie.title}} ({{movie2.date}})</h2><hr>
+          <p> {{movie.overview}} </p><hr>
+          <genres v-bind:genres="genres" /> <hr>
+          <h3><b-icon icon="star-fill" variant="warning">
+            </b-icon>{{" " + movie.vote_average}}<h6>Votos: {{movie.vote_count}}</h6>
+          </h3> 
+          <a :href="movie.homepage">Página oficial de la película</a>
+        </b-card-text>
+      </b-card>
 
-    <b-card>
-      <b-card-text>
-        <h6>Título original: {{movie.original_title}}</h6>
-        <h6>Lenguaje original: {{movie.original_language}}</h6>
-        <h6>Duración: {{movie.runtime}} minutos </h6>
-        <h6>Fecha estreno: {{movie2.allDate}}</h6>
-        <h6>Presupuesto (USD): $ {{movie2.budget}}</h6>
-        <h6>Ingresos (USD): $ {{movie2.revenue}}</h6>
-        <hr><h4>Compañias productoras:</h4><hr>
-        <genres v-bind:genres="p_companies" />
-        <hr><h4>Paises productoras:</h4><hr>
-        <genres v-bind:genres="p_countries" /> <hr>
-      </b-card-text>
-    </b-card>
+      <b-card>
+        <b-card-text>
+          <h6>Título original: {{movie.original_title}}</h6>
+          <h6>Lenguaje original: {{movie.original_language}}</h6>
+          <h6>Duración: {{movie.runtime}} minutos </h6>
+          <h6>Fecha estreno: {{movie2.allDate}}</h6>
+          <h6>Presupuesto (USD): $ {{movie2.budget}}</h6>
+          <h6>Ingresos (USD): $ {{movie2.revenue}}</h6>
+          <hr><h4>Compañias productoras:</h4><hr>
+          <genres v-bind:genres="p_companies" />
+          <hr><h4>Paises productoras:</h4><hr>
+          <genres v-bind:genres="p_countries" /> <hr>
+        </b-card-text>
+      </b-card>
+    </div>
+    <div v-if="!mostrar">
+      <h1>No existe el sitio al cual quiere acceder.</h1>
+    </div>
   </div>
 </template>
 
@@ -45,6 +50,7 @@ export default {
       movie2: {},
       p_companies: [],
       p_countries: [],
+      mostrar: false,
     }
   },
   created(){
@@ -53,6 +59,7 @@ export default {
   methods:{
     async datosPelicula(){
       let movie = await axios.get('https://api.themoviedb.org/3/movie/'+this.id+'?api_key='+this.api_key+'&language=es-MX');
+      this.mostrar = true;
       //console.log(movie.data);
       this.movie = movie.data;
       this.movie2['poster'] = this.base_img + movie.data.poster_path;
