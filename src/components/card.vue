68488e84b5cf58bfa4ef515e849f677703fa3a84
Danilo <ibanez3497@gmail.com>
2020-01-22 20:55:46 -0300

    Card responsive

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
+    <!--
     <b-card :img-src="movies.poster" img-alt="Card image" img-left class="mb-3" style="cursor:pointer;" img-width="150" v-on:click="movie(movies.id)">
-        <b-card-text>
-          <h2>{{movies.title}} ({{movies.date.slice(0,4)}})</h2><hr>
-          <!-- <p>{{movies[index].overview}}</p> -->
-          <genres v-bind:genres="movies.geners" /> <hr>  
-          <h3><b-icon icon="star-fill" variant="warning">
-             </b-icon>{{" " + movies.vote_average}}<h6>Votos: {{movies.vote_count}}</h6>
-          </h3>
-        </b-card-text>
+      <b-card-text>
+        <h2>{{movies.title}} ({{movies.date.slice(0,4)}})</h2><hr>
+        <genres v-bind:genres="movies.geners" /> <hr>  
+        <h3><b-icon icon="star-fill" variant="warning">
+            </b-icon>{{" " + movies.vote_average}}<h6>Votos: {{movies.vote_count}}</h6>
+        </h3>
+      </b-card-text>
+    </b-card>
+    -->
+    <div>
+      <b-card no-body class="mb-3" style="cursor:pointer;" v-on:click="movie(movies.id)">
+        <b-row no-gutters>
+          <b-col md="2">
+            <b-card-img :src="movies.poster" style="max-width: 200px;" class="rounded-0"></b-card-img>
+          </b-col>
+          <b-col md="10">
+            <b-card-text class="mt-3">
+              <h2>{{movies.title}} ({{movies.date.slice(0,4)}})</h2><hr>
+              <genres v-bind:genres="movies.geners" /> <hr>  
+              <h3><b-icon icon="star-fill" variant="warning">
+                  </b-icon>{{" " + movies.vote_average}}<h6>Votos: {{movies.vote_count}}</h6>
+              </h3>
+            </b-card-text>
+          </b-col>
+        </b-row>
       </b-card>
+    </div>
   </div>
 </template>
 
@@ -24,10 +43,12 @@ export default {
   data(){
     return{
       movies: {},
+      title: '',
     }
   },
   created() {
     this.movies = this.data_card;
+    this.title = this.movies.title + ' (' + this.movies.date.slice(0,4) + ')';
   },
   methods: {
     movie(id){
@@ -36,3 +57,4 @@ export default {
   },
 }
 </script>
+
