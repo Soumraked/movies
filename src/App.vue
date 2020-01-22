036d1854919458ac5a76297d8a43923985e55b53
Nicolas <nikkolasnv1997@gmail.com>
2020-01-22 15:34:10 -0300

    ultimos detalles graficos

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,30 @@
 
     <div>
       <b-navbar toggleable="lg" type="dark" variant="info">
-        
-        <img class="mr-4" src="./assets/image.png" width="50px" id="img_logomaster">
-        <b-navbar-brand>MejoresMovies</b-navbar-brand>
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <div class="container">
+          <img class="mr-4" src="./assets/image.png" width="50px" id="img_logomaster">
+          <b-navbar-brand>MejoresPelis</b-navbar-brand>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item active v-if="this.$route.name == 'populares'"  href="/">Populares</b-nav-item>
-            <b-nav-item v-if="this.$route.name != 'populares'" href="/">Populares</b-nav-item>
-          </b-navbar-nav>
-
-            <b-navbar-nav class="ml-auto">
-              <b-nav-form>
-                    <b-form-input autocomplete="off" size="sm" v-model="busqueda" type="text" placeholder="Buscar" v-on:change="buscar()" debounce="5" ></b-form-input>
-                    <b-input-group-append>
-                        <b-button size="sm" v-on:click="buscar()" variant="dark"><b-icon icon="search"></b-icon></b-button>
-                    </b-input-group-append>
-                <b-form-input style="display:none;"></b-form-input>
-              </b-nav-form>
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item active v-if="this.$route.name == 'populares'"  href="/">Populares</b-nav-item>
+              <b-nav-item v-if="this.$route.name != 'populares'" href="/">Populares</b-nav-item>
             </b-navbar-nav>
 
-          </b-collapse>
+              <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                      <b-form-input autocomplete="off" size="sm" v-model="busqueda" type="text" placeholder="Buscar" v-on:change="buscar()" debounce="5" ></b-form-input>
+                      <b-input-group-append>
+                          <b-button size="sm" v-on:click="buscar()" variant="dark"><b-icon icon="search"></b-icon></b-button>
+                      </b-input-group-append>
+                  <b-form-input style="display:none;"></b-form-input>
+                </b-nav-form>
+              </b-navbar-nav>
+
+            </b-collapse>
+          </div>
+          
       </b-navbar>
     </div>
     <router-view/>
